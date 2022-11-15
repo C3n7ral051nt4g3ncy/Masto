d7f3150879689d3d79d7dab79697893508715b77
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-15 07:26:57 +0100

    created a class for all functions

--- masto.py
+++ masto.py
@@ -19,250 +19,255 @@ from pprint import pprint
 from w3lib.html import remove_tags
 
 
-# banner
-print(
-    """\033[34m
-███╗   ███╗ █████╗ ███████╗████████╗ ██████╗
-████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔═══██╗
-██╔████╔██║███████║███████╗   ██║   ██║   ██║
-██║╚██╔╝██║██╔══██║╚════██║   ██║   ██║   ██║
-██║ ╚═╝ ██║██║  ██║███████║   ██║   ╚██████╔╝
-╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝    ╚═════╝v2.0\033[0m"""
-)
-print("\033[34m\033[1mMastodon OSINT Tool\033[0m")
-print("by @C3n7ral051nt4g3ncy ")
-print(
-    "https://ko-fi.com/tacticalintelanalyst\n"
-    "BTC: bc1q66awg48m2hvdsrf62pvev78z3vkamav7chusde\n"
-)
+class Masto:
+    def __init__(self) -> None:
+        self.__intro()
 
-# search mastodon instances (servers)
-def instance_search(instance):
-    headers = {
-        "Accept": "text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "accept-language": "en-US;q=0.9,en,q=0,8",
-        "accept-encoding": "gzip, deflate",
-        "user-Agent": "Mozilla/5.0 (Windows NT 10.0;Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) "
-        "Chrome/104.0.0.0 Safari/537.36",
-    }
-    inst_url = f"https://{instance}/api/v1/instance.json"
-    try:
-        response = requests.request("GET", inst_url, headers=headers)
-        inst_data = json.loads(response.text)
-    except Exception as e:
-        inst_data = {}
-    for _ in tqdm(range(10)):
-        time.sleep(0.03)
-    if not inst_data:
+    def __intro(self):
+
+        # banner
         print(
-            f"\n\033[31mMastodon instance\033[1m [{instance}]\033[0m\033[31m NOT found!\033[0m"
+            """\033[34m
+        ███╗   ███╗ █████╗ ███████╗████████╗ ██████╗
+        ████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔═══██╗
+        ██╔████╔██║███████║███████╗   ██║   ██║   ██║
+        ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██║   ██║
+        ██║ ╚═╝ ██║██║  ██║███████║   ██║   ╚██████╔╝
+        ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝    ╚═════╝v2.0\033[0m"""
         )
-        return
-    name = inst_data["uri"]
-    print("\ninstance (server): ", name)
-    title = inst_data["title"]
-    print("title: \033[32m\033[1m", title)
-    s_description = inst_data["short_description"]
-    s_description = remove_tags(s_description)
-    print("\033[0mdescription: ", s_description)
-    det_descript = inst_data["description"]
-    invalid_tags = [
-        "<strong>",
-        "</strong>",
-        "<a href=",
-        "target=",
-        "</a>",
-        "<a>",
-        "_blank",
-        ">",
-        "<",
-        '"',
-        "br /",
-        "|",
-    ]
-    for invalid_tag in invalid_tags:
-        det_descript = det_descript.replace(invalid_tag, "")
-    print("detailed description: ", det_descript)
-    e_mail = inst_data["email"]
-    print("\ninstance email: \033[32m\033[1m", e_mail)
-    thumb = inst_data["thumbnail"]
-    print("\033[0mserver thumbnail:", thumb)
-    lang = inst_data["languages"]
-    print("instance languages: ", lang)
-    reg = inst_data["registrations"]
-    print("registration needed: ", reg)
-    reg_approve = inst_data["approval_required"]
-    print("admin approval required: ", reg_approve)
-    print("\ninstance admin information:")
-    admin_data = inst_data["contact_account"]
-    for key in [
-        "id",
-        "username",
-        "acct",
-        "display_name",
-        "followers_count",
-        "following_count",
-        "statuses_count",
-        "last_status_at",
-        "locked",
-        "bot",
-        "discoverable",
-        "group",
-        "created_at",
-        "url",
-        "avatar",
-        "header",
-    ]:
-        print(f"{key}: {admin_data[key]}")
-
-
-# search username with Mastodon API
-def username_search_api(username):
-    url = f"https://mastodon.social/api/v2/search?q={username}"
-    response = requests.request("GET", url)
-    data = json.loads(response.text)
-
-    for _ in tqdm(range(10)):
-        time.sleep(0.03)
-
-    if response.text == ('{"accounts":[],"statuses":[],"hashtags":[]}'):
+        print("\033[34m\033[1mMastodon OSINT Tool\033[0m")
+        print("by @C3n7ral051nt4g3ncy ")
         print(
-            f"\n\033[1m\033[31mTarget username: [{username}] NOT found using the Mastodon API!\033[0m"
+            "https://ko-fi.com/tacticalintelanalyst\n"
+            "BTC: bc1q66awg48m2hvdsrf62pvev78z3vkamav7chusde\n"
         )
-        return
-    time.sleep(1)
 
-    data = filter(
-        lambda x: x.get("username").lower() == username.lower(), data["accounts"]
-    )
-    for index, intelligence in enumerate(list(data), start=1):
-
-        print("\n\n══════════")
-        print(f"\033[32m\033[1mAccount: {index}\033[0m")
-        print("══════════\n")
-
-        identity = intelligence["id"]
-        lock = intelligence["locked"]
-        pro_url = intelligence["url"]
-        target_username = intelligence["username"]
-        account = intelligence["acct"]
-        dispn = intelligence["display_name"]
-        creation_date = intelligence["created_at"]
-        bot = intelligence["bot"]
-        dscvr = intelligence["discoverable"]
-        fwers = intelligence["followers_count"]
-        fwing = intelligence["following_count"]
-        posts = intelligence["statuses_count"]
-        lastm = intelligence["last_status_at"]
-        group = intelligence["group"]
-        note = intelligence["note"]
-        avt = intelligence["avatar"]
-
-        print("user ID:\033[32m\033[1m", identity)
-        print("\033[0mprofile url:", pro_url)
-        print("account locked:", lock)
-        print("username:", target_username)
-        print("\033[0maccount:\033[32m\033[1m", account)
-        print("\033[0mdisplay Name:\033[32m\033[1m", dispn)
-        print("\033[0mprofile creation date:", creation_date)
-        print("user is a bot:", bot)
-        print("user opted to be listed on the profile directory:", dscvr)
-        print("followers:", fwers)
-        print("following:", fwing)
-        print("number of posts:", posts)
-        print("user last message date:", lastm)
-        print("user is a group:", group)
-
-        bad_tags = [
-            "<p>",
-            "</p>",
-            "</a>",
-            "</span>",
-            "<span>",
-            "<a href",
-            '"',
-            "<",
-            ">",
-            "class=",
-            "rel=tag",
-            "=",
-            "relnofollow",
-            "noopener",
-            "noreferrer",
-            "target_blankspan",
-            "target_blank",
-            "span",
-            "invisible",
-            "\u003e\u003c/a\u003e.",
-            "\u003cbr",
-        ]
-        for bad_tag in bad_tags:
-            note = note.replace(bad_tag, "")
-        print("user bio:", note)
-        print("user's avatar link:", avt)
-
-        choice = input("\033[1mopen avatar in browser | [Y|N]: \033[0m")
-        if choice in ["y", "Y", "YES", "yes"]:
-            webbrowser.open(avt)
-        if choice in ["n", "N", "NO", "no"]:
-            continue
-
-
-# username search with the Masto OSINT Tool servers database
-def username_search(username):
-    print("\n")
-    print(
-        f"Preparing to scan for target -->\033[32m\033[1m {username}\033[0m on the \033[32m\033[1m"
-        f"Masto OSINT Tool servers database\033[0m\033[0m\n"
-    )
-    time.sleep(6)
-    for _ in tqdm(range(10)):
-        time.sleep(0.03)
-    headers = {
-        "Accept": "text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "accept-language": "en-US;q=0.9,en,q=0,8",
-        "accept-encoding": "gzip, deflate",
-        "user-Agent": "Mozilla/5.0 (Windows NT 10.0;Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) "
-        "Chrome/104.0.0.0 Safari/537.36",
-    }
-
-    response = requests.get(
-        "https://raw.githubusercontent.com/C3n7ral051nt4g3ncy/Masto/master/fediverse_instances.json"
-    )
-    sites = response.json()["sites"]
-    is_any_site_matched = False
-    for site in sites:
-        uri_check = site["uri_check"]
-        site_name = site["name"]
-        uri_check = uri_check.format(account=username)
-
+    # search mastodon instances (servers)
+    def instance_search(self, instance):
+        headers = {
+            "Accept": "text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "accept-language": "en-US;q=0.9,en,q=0,8",
+            "accept-encoding": "gzip, deflate",
+            "user-Agent": "Mozilla/5.0 (Windows NT 10.0;Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) "
+            "Chrome/104.0.0.0 Safari/537.36",
+        }
+        inst_url = f"https://{instance}/api/v1/instance.json"
         try:
-            res = requests.get(uri_check, headers=headers)
-
-            estring_pos = res.text.find(site["e_string"]) > 0
-
+            response = requests.request("GET", inst_url, headers=headers)
+            inst_data = json.loads(response.text)
         except Exception as e:
-            continue
-
-        if res.status_code == 200 and estring_pos:
-            is_any_site_matched = True
-            print("\033[32m-" * 77)
+            inst_data = {}
+        for _ in tqdm(range(10)):
+            time.sleep(0.03)
+        if not inst_data:
             print(
-                f"\033[32m[+] \033[1mTarget found\033[0m\033[32m ✓ on:\033[1m{site_name}\033[0m"
+                f"\n\033[31mMastodon instance\033[1m [{instance}]\033[0m\033[31m NOT found!\033[0m"
             )
-            print(f"\033[32m[+] Profile URL:\033[1m{uri_check}\033[0m")
-            print("\033[32m\033[1m-\033[0m" * 77)
+            return
+        name = inst_data["uri"]
+        print("\ninstance (server): ", name)
+        title = inst_data["title"]
+        print("title: \033[32m\033[1m", title)
+        s_description = inst_data["short_description"]
+        s_description = remove_tags(s_description)
+        print("\033[0mdescription: ", s_description)
+        det_descript = inst_data["description"]
+        invalid_tags = [
+            "<strong>",
+            "</strong>",
+            "<a href=",
+            "target=",
+            "</a>",
+            "<a>",
+            "_blank",
+            ">",
+            "<",
+            '"',
+            "br /",
+            "|",
+        ]
+        for invalid_tag in invalid_tags:
+            det_descript = det_descript.replace(invalid_tag, "")
+        print("detailed description: ", det_descript)
+        e_mail = inst_data["email"]
+        print("\ninstance email: \033[32m\033[1m", e_mail)
+        thumb = inst_data["thumbnail"]
+        print("\033[0mserver thumbnail:", thumb)
+        lang = inst_data["languages"]
+        print("instance languages: ", lang)
+        reg = inst_data["registrations"]
+        print("registration needed: ", reg)
+        reg_approve = inst_data["approval_required"]
+        print("admin approval required: ", reg_approve)
+        print("\ninstance admin information:")
+        admin_data = inst_data["contact_account"]
+        for key in [
+            "id",
+            "username",
+            "acct",
+            "display_name",
+            "followers_count",
+            "following_count",
+            "statuses_count",
+            "last_status_at",
+            "locked",
+            "bot",
+            "discoverable",
+            "group",
+            "created_at",
+            "url",
+            "avatar",
+            "header",
+        ]:
+            print(f"{key}: {admin_data[key]}")
 
-    if not is_any_site_matched:
-        print(
-            f"\n\033[1m\033[31mTarget username: [{username}] NOT found on the Masto OSINT Tool servers database!\033[0m"
+    # search username with Mastodon API
+    def username_search_api(self, username):
+        url = f"https://mastodon.social/api/v2/search?q={username}"
+        response = requests.request("GET", url)
+        data = json.loads(response.text)
+
+        for _ in tqdm(range(10)):
+            time.sleep(0.03)
+
+        if response.text == ('{"accounts":[],"statuses":[],"hashtags":[]}'):
+            print(
+                f"\n\033[1m\033[31mTarget username: [{username}] NOT found using the Mastodon API!\033[0m"
+            )
+            return
+        time.sleep(1)
+
+        data = filter(
+            lambda x: x.get("username").lower() == username.lower(), data["accounts"]
         )
-    return is_any_site_matched
+        for index, intelligence in enumerate(list(data), start=1):
+
+            print("\n\n══════════")
+            print(f"\033[32m\033[1mAccount: {index}\033[0m")
+            print("══════════\n")
+
+            identity = intelligence["id"]
+            lock = intelligence["locked"]
+            pro_url = intelligence["url"]
+            target_username = intelligence["username"]
+            account = intelligence["acct"]
+            dispn = intelligence["display_name"]
+            creation_date = intelligence["created_at"]
+            bot = intelligence["bot"]
+            dscvr = intelligence["discoverable"]
+            fwers = intelligence["followers_count"]
+            fwing = intelligence["following_count"]
+            posts = intelligence["statuses_count"]
+            lastm = intelligence["last_status_at"]
+            group = intelligence["group"]
+            note = intelligence["note"]
+            avt = intelligence["avatar"]
+
+            print("user ID:\033[32m\033[1m", identity)
+            print("\033[0mprofile url:", pro_url)
+            print("account locked:", lock)
+            print("username:", target_username)
+            print("\033[0maccount:\033[32m\033[1m", account)
+            print("\033[0mdisplay Name:\033[32m\033[1m", dispn)
+            print("\033[0mprofile creation date:", creation_date)
+            print("user is a bot:", bot)
+            print("user opted to be listed on the profile directory:", dscvr)
+            print("followers:", fwers)
+            print("following:", fwing)
+            print("number of posts:", posts)
+            print("user last message date:", lastm)
+            print("user is a group:", group)
+
+            bad_tags = [
+                "<p>",
+                "</p>",
+                "</a>",
+                "</span>",
+                "<span>",
+                "<a href",
+                '"',
+                "<",
+                ">",
+                "class=",
+                "rel=tag",
+                "=",
+                "relnofollow",
+                "noopener",
+                "noreferrer",
+                "target_blankspan",
+                "target_blank",
+                "span",
+                "invisible",
+                "\u003e\u003c/a\u003e.",
+                "\u003cbr",
+            ]
+            for bad_tag in bad_tags:
+                note = note.replace(bad_tag, "")
+            print("user bio:", note)
+            print("user's avatar link:", avt)
+
+            choice = input("\033[1mopen avatar in browser | [Y|N]: \033[0m")
+            if choice in ["y", "Y", "YES", "yes"]:
+                webbrowser.open(avt)
+            if choice in ["n", "N", "NO", "no"]:
+                continue
+
+    # username search with the Masto OSINT Tool servers database
+    def username_search(self, username):
+        print("\n")
+        print(
+            f"Preparing to scan for target -->\033[32m\033[1m {username}\033[0m on the \033[32m\033[1m"
+            f"Masto OSINT Tool servers database\033[0m\033[0m\n"
+        )
+        time.sleep(6)
+        for _ in tqdm(range(10)):
+            time.sleep(0.03)
+        headers = {
+            "Accept": "text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "accept-language": "en-US;q=0.9,en,q=0,8",
+            "accept-encoding": "gzip, deflate",
+            "user-Agent": "Mozilla/5.0 (Windows NT 10.0;Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) "
+            "Chrome/104.0.0.0 Safari/537.36",
+        }
+
+        response = requests.get(
+            "https://raw.githubusercontent.com/C3n7ral051nt4g3ncy/Masto/master/fediverse_instances.json"
+        )
+        sites = response.json()["sites"]
+        is_any_site_matched = False
+        for site in sites:
+            uri_check = site["uri_check"]
+            site_name = site["name"]
+            uri_check = uri_check.format(account=username)
+
+            try:
+                res = requests.get(uri_check, headers=headers)
+
+                estring_pos = res.text.find(site["e_string"]) > 0
+
+            except Exception as e:
+                continue
+
+            if res.status_code == 200 and estring_pos:
+                is_any_site_matched = True
+                print("\033[32m-" * 77)
+                print(
+                    f"\033[32m[+] \033[1mTarget found\033[0m\033[32m ✓ on:\033[1m{site_name}\033[0m"
+                )
+                print(f"\033[32m[+] Profile URL:\033[1m{uri_check}\033[0m")
+                print("\033[32m\033[1m-\033[0m" * 77)
+
+        if not is_any_site_matched:
+            print(
+                f"\n\033[1m\033[31mTarget username: [{username}] NOT found on the Masto OSINT Tool servers database!\033[0m"
+            )
+        return is_any_site_matched
 
 
 # main
 if __name__ == "__main__":
 
+    masto = Masto()
     # argparse arguments
     parser = argparse.ArgumentParser(
         description="Masto OSINT Tool help --> "
@@ -283,6 +288,12 @@ if __name__ == "__main__":
         help="\033[32m\033[1m\ninstance (server)\033[0m",
     )
 
+    parser.add_argument(
+        "-s",
+        "--silent",
+        help="\033[32m\033[1m\nsilent mode, just print the results\033[0m",
+    )
+
     # args settings
     args = parser.parse_args()
 
@@ -298,7 +309,7 @@ if __name__ == "__main__":
     # Loop Instance Search
     if instance:
         while True:
-            instance_found = instance_search(instance)
+            instance_found = masto.instance_search(instance)
 
             yes_no = input("\nCheck another instance? [yes|no]: ")
             if yes_no.lower() == "no":
@@ -309,8 +320,8 @@ if __name__ == "__main__":
     # Loop target username search across all instances with API and Masto OSINT tool database
     if username:
         while True:
-            api_user_found = username_search_api(username)
-            user_found = username_search(username)
+            api_user_found = masto.username_search_api(username)
+            user_found = masto.username_search(username)
 
             yes_no = input("\nTry another username? [yes|no]: ")
             if yes_no.lower() == "no":
